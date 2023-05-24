5d25833daa51b465adf1979b00980c142c137222
Taii <126676872+oliveiratainara948@users.noreply.github.com>
2023-05-24 16:56:43 -0300

    commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+let btn1 = 0;
+let btn2 = 0;
+let btn3 = 0;
+let btn4 = 0;
+let btn5 = 0;
+let btn6 = 0;
+let btn7 = 0;
+let btn8 = 0;
+let btn9 = 0;
+let controle = 0;
+let jogadas = 0;
+let finalJogo = false ;
+
+// Click botão 1
+function clickBtn1(){
+    let btn = document.getElementById('btn1'); 
+    if (btn.value == ' ' && !finalJogo){
+        if (controle == 0){
+        controle = 1;
+        btn.value = "x";
+        btn1 = "x";
+        } else {
+            controle = 0;
+            btn.value =  "O";
+            btn1 = "O";
+        }
+
+    }
+}
